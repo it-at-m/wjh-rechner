5ca6040737837034a5d4e1193fd4498d4a789710
Max Zollbrecht <m.zollbrecht@muenchen.de>
2024-02-28 14:39:34 +0100

    mietobergrenzen - flaeche entfernen

--- wjh/src/constants.ts
+++ wjh/src/constants.ts
@@ -1,28 +1,25 @@
-export const todoVariable = 'todo-value'
-
 export const grundbetrag = 1182
 export const familienzuschlag = 414
-export const getGrundbetragMitFamilie = (persons : number) : number => {
-    return grundbetrag + (persons ?? 0) * familienzuschlag;
+export const getGrundbetragMitFamilie = (personen : number) : number => {
+    return grundbetrag + (personen ?? 0) * familienzuschlag;
 }
 
 export const mietobergrenzen = [
-  { persons: 1, area: 50, miete: 849 },
-  { persons: 2, area: 65, miete: 1201 },
-  { persons: 3, area: 75, miete: 1415 },
-  { persons: 4, area: 90, miete: 1726 },
-  { persons: 5, area: 105, miete: 2133 },
-  { persons: 6, area: 120, miete: 2407 }
+  { personen: 1, miete: 849 },
+  { personen: 2, miete: 1201 },
+  { personen: 3, miete: 1415 },
+  { personen: 4, miete: 1726 },
+  { personen: 5, miete: 2133 },
+  { personen: 6, miete: 2407 }
 ]
-export const getMietobergrenze = (persons : number) => {
-    if (persons <= 6) {
-      return mietobergrenzen[persons-1];
+export const getMietobergrenze = (personen : number) => {
+    if (personen <= 6) {
+      return mietobergrenzen[personen-1];
     } else {
       // letzten Eintrag auswählen und für zusätzliche Personen erhöhen
       const mietobergrenze = mietobergrenzen[5]
-      const additionalPersons = persons - 6
-      mietobergrenze.area += additionalPersons * 15
-      mietobergrenze.miete += additionalPersons * 310
+      const zusaetzlichePersonen = personen - 6
+      mietobergrenze.miete += zusaetzlichePersonen * 310
       return mietobergrenze;
     }
   }
