8bb08fee5e717c00435d12193081937dc27dc0ce
Max Zollbrecht <m.zollbrecht@muenchen.de>
2024-02-28 14:36:41 +0100

    nicht verwendete Dateien entfernen

--- wjh/src/api/generatedTypes.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2023-06-28 16:35:31.
-
-export class GetCodeDto {
-  mail?: string
-  topfName?: string
-}
