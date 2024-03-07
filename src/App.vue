008db5c8fab5cc574b9876af337acccdcf4aaa23
eidottermihi <eidottermihi@gmail.com>
2024-03-07 10:08:38 +0100

    use GitHub logo, add slight padding

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
   <v-app id="wjh-rechner-app">
-    <h2 class="px-4 pt-1">{{ $t("app.name") }}</h2>
+    <h2 class="px-4 pt-1">{{ $t('app.name') }}</h2>
     <einverstaendnisabfrage v-model="confirmConsent" />
     <wjh-eingabe v-model="userDataValue" :is-mobile="isMobile" :is-active="!confirmConsent" />
-    <a class="impressum" href="https://stadt.muenchen.de/infos/impressum-datenschutz.html" target="_blank">
-      {{ $t("app.impressum") }}
+    <a class="impressum" :href="impressLink" target="_blank">
+      {{ $t('app.impressum') }}
     </a>
-    <a class="github-link" :title="$t('app.repository')" :href="impressumLink" target="_blank">
-      <v-icon>mdi-git</v-icon>
+    <a class="github-link" :title="$t('app.repository')" :href="githubRepoLink" target="_blank">
+      <v-img :src="githubImgUrl" height="24" width="24" />
     </a>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import { impressumLink } from '@/constants'
-import WjhEingabe from '@/components/WjhEingabe.vue';
-import Einverstaendnisabfrage from '@/components/Einverstaendnisabfrage.vue';
+import { githubRepoLink, impressLink } from '@/constants'
+import WjhEingabe from '@/components/WjhEingabe.vue'
+import Einverstaendnisabfrage from '@/components/Einverstaendnisabfrage.vue'
 import { ref } from 'vue'
-import { UserData } from './api/wjhTypes';
+import { UserData } from './api/wjhTypes'
+import githubImgUrl from '@/assets/github-mark.png'
 
 const confirmConsent = ref(true)
 const userDataValue = ref(new UserData())
 
-const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  navigator.userAgent
+)
 </script>
 
 <style>
 #wjh-rechner-app {
-  color: #3A5368;
+  color: #3a5368;
 }
 
 .pointer {
@@ -48,13 +51,13 @@ const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/
 
 .impressum {
   position: fixed;
-  bottom: 0px;
+  bottom: 4px;
   left: 4px;
 }
 
 .github-link {
   position: fixed;
-  bottom: 0px;
-  right: 0px;
+  bottom: 4px;
+  right: 4px;
 }
 </style>
--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
-export const impressumLink = "https://github.com/it-at-m/wjh-rechner"
+export const githubRepoLink = 'https://github.com/it-at-m/wjh-rechner'
+export const impressLink = 'https://stadt.muenchen.de/infos/impressum-datenschutz.html'
 
 export const grundbetrag = 1182
 export const familienzuschlag = 414
 export const nebenkostenProQm = 1.2
-export const getGrundbetragMitFamilie = (personen : number) : number => {
-    return grundbetrag + (personen ? personen-1 : 0) * familienzuschlag;
+export const getGrundbetragMitFamilie = (personen: number): number => {
+  return grundbetrag + (personen ? personen - 1 : 0) * familienzuschlag
 }
 
 export const mietobergrenzen = [
@@ -15,14 +16,14 @@ export const mietobergrenzen = [
   { personen: 5, miete: 2133 },
   { personen: 6, miete: 2407 }
 ]
-export const getMietobergrenze = (personen : number) => {
-    if (personen <= 6) {
-      return mietobergrenzen[personen-1];
-    } else {
-      // letzten Eintrag auswählen und für zusätzliche Personen erhöhen
-      const mietobergrenze = mietobergrenzen[5]
-      const zusaetzlichePersonen = personen - 6
-      mietobergrenze.miete += zusaetzlichePersonen * 310
-      return mietobergrenze;
-    }
+export const getMietobergrenze = (personen: number) => {
+  if (personen <= 6) {
+    return mietobergrenzen[personen - 1]
+  } else {
+    // letzten Eintrag auswählen und für zusätzliche Personen erhöhen
+    const mietobergrenze = mietobergrenzen[5]
+    const zusaetzlichePersonen = personen - 6
+    mietobergrenze.miete += zusaetzlichePersonen * 310
+    return mietobergrenze
   }
+}
